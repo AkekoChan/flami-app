9c048f9af069c35e5ea5c2cce1e2466064a927d3
Maxence <maxence.gvr@gmail.com>
2024-02-08 22:20:52 +0100

    Update user.model.js

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,19 +4,55 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    unique: true
   },
   email: {
     type: String,
     required: true,
+    unique: true
   },
   password: {
     type: String,
-    required: true,
+    required: true
   },
   date: {
     type: Date,
-    default: Date.now,
+    default: Date.now
   },
+  age: {
+    type: Number,
+    required: true,
+    min: 13
+  },
+  flami_id: {
+    type: Number,
+    required: true
+  },
+  badges: {
+    type: Array,
+    required: true
+  },
+  metadata: [{
+    favorite_sport: {
+      type: String,
+      required: true
+    },
+    origin: {
+      type: String,
+      required: true
+    },
+    intent: {
+      type: String,
+      required: true
+    }
+  }]
+}, 
+{ 
+  statics: {
+    findByEmail (email) {
+      return this.find({ email: new RegExp(email, 'i') });
+    }
+  }
 });
 
 export default mongoose.model("User", userSchema);
