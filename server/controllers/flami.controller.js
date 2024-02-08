8691ac8901ea14eb201bd2cda29eeae0908131d4
Maxence <maxence.gvr@gmail.com>
2024-02-08 22:20:48 +0100

    Update controllers

---
+++ server/controllers/flami.controller.js
@@ -0,0 +1,17 @@
+const flamiController = {
+    getFlami: (req, res) => {
+
+    },
+    getCosmetics: (req, res) => {
+
+    },
+    share: (req, res) => {
+
+    },
+    rankup: (req, res) => {
+
+    }
+};
+
+export default flamiController;
+  
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,24 +1,16 @@
-import userModel from "../models/user.model.js";
-
 const userController = {
-  getUsers: async (req, res) => {
-    try {
-      const users = await userModel.find();
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(404).json({ message: error.message });
+    getProfile: (req, res) => {
+
+    },
+    getAccount: (req, res) => {
+        
+    },
+    getBadges: (req, res) => {
+        
+    },
+    updateAccount: (req, res) => {
+        
     }
-  },
-  addUser: async (req, res) => {
-    const user = req.body;
-    const newUser = new userModel(user);
-    try {
-      await newUser.save();
-      res.status(201).json(newUser);
-    } catch (error) {
-      res.status(409).json({ message: error.message });
-    }
-  },
 };
 
 export default userController;
