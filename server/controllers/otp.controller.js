b42cdc93d0be612a47c7e0b2c2209985e073a327
Akeko <artapp.theo@gmail.com>
2024-02-23 13:55:47 +0100

    refactor(auth): change name controller otp

--- server/controllers/otp.controller.js
+++ server/controllers/otp.controller.js
@@ -1,7 +1,7 @@
 import otpGenerator from "otp-generator";
 import OTP from "../models/otp.model.js";
 
-const optController = {
+const otpController = {
   setOTP: async (req, res) => {
     try {
       const { email } = req.body;
@@ -14,18 +14,20 @@ const optController = {
 
       const result = await OTP.findOne({ otp: otp });
 
+      // try catch email check
       while (result) {
+        console.log(result);
         otp = otpGenerator.generate(6, {
           upperCaseAlphabets: false,
         });
       }
+
       const otpPayload = { email, otp };
       await OTP.create(otpPayload);
 
       res.status(200).json({
         message: `OTP envoyé.`,
       });
-
     } catch (error) {
       console.log(error.message);
       return res.status(500).json({ error: error.message });
@@ -33,4 +35,4 @@ const optController = {
   },
 };
 
-export default optController;
+export default otpController;
