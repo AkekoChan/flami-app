ef3397a60681008d9b831b84c60db59ae9847098
Maxence <maxence.gvr@gmail.com>
2024-03-11 11:43:31 +0100

    update(models): updated flami + user models

--- server/models/flami.model.js
+++ server/models/flami.model.js
@@ -1,7 +1,48 @@
 import mongoose from "mongoose";
 
 const flamiShema = new mongoose.Schema({
-  
+  name: {
+    type: String
+  },
+  owner: {
+    type: mongoose.Types.ObjectId
+  },
+  current_friend: {
+    type: mongoose.Types.ObjectId,
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  cosmetics: {
+    type: Array,
+    default: []
+  },
+  stats: {
+    strength: {
+        type: Number,
+        default: 0
+    },
+    speed: {
+        type: Number,
+        default: 0
+    },
+    dexterity: {
+        type: Number,
+        default: 0
+    }
+  },
+  stamina: {
+    type: Number,
+    default: 3
+  },
+  last_action_time: {
+    type: Date
+  }
+}, {
+    statics: {
+
+    }
 });
 
 export default mongoose.model("Flami", flamiShema);
