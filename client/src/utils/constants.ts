28f18454be41f747e2bc12e6bee8805fa29c0c1e
Akeko <artapp.theo@gmail.com>
2024-03-11 13:14:37 +0100

    refactor(api): change handler api

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,5 +1,1 @@
 export const RE_DIGIT = new RegExp(/^\d+$/);
-
-export const URL_API_FLAMI = "http://localhost:3001/api";
-export const URL_API_MAP = "https://maksance.alwaysdata.net/api-jo";
-export const URL_API_KEY = "11f66865-de4c-42a3-9728-851fa6fcda85";
