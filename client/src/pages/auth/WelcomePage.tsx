c0ad42b06006b6bfc3ed1596ed7d17f82ff3e80c
Akeko <artapp.theo@gmail.com>
2024-03-13 17:11:02 +0100

    refactor(pwa): add install btn and manisfest

--- client/src/pages/auth/WelcomePage.tsx
+++ client/src/pages/auth/WelcomePage.tsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 import { Button, LinkComponent } from "../../components/ui";
+import { usePwa } from "@dotmind/react-use-pwa";
+import { useCallback } from "react";
 
 const WelcomePage = () => {
+  const { installPrompt, isInstalled, canInstall } = usePwa();
+
+  const handleInstallPrompt = useCallback(() => {
+    if (canInstall) {
+      installPrompt();
+    }
+  }, [canInstall, installPrompt]);
+
   return (
     <section className="flex flex-col justify-center gap-8 h-full">
       <div className="flex flex-col gap-4">
@@ -16,7 +26,14 @@ const WelcomePage = () => {
         <LinkComponent variant={"primary"} to="/sign-up">
           Créer son compte
         </LinkComponent>
-        <Button variant={"secondary"}>Télécharger l'application</Button>
+        {canInstall || !isInstalled ? (
+          <Button variant={"secondary"} onClick={handleInstallPrompt}>
+            Télécharger l'application
+          </Button>
+        ) : (
+          ""
+        )}
+
         <p className="text-center">
           Tu as deja un compte ?{" "}
           <Link
