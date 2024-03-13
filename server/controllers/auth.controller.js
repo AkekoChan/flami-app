d98998cb099dd73fc813f5ce70014b759ed2d33c
Akeko <artapp.theo@gmail.com>
2024-03-13 11:44:11 +0100

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -106,7 +106,7 @@ const authController = {
 
       if (isValid) {
         let token = auth.encode({ email: user.email });
-        req.brute.reset(); // reset brute counter
+        // req.brute.reset();
         return res.status(200).json({
           data: {
             message: "Bienvenue sur l'application Flami !",
