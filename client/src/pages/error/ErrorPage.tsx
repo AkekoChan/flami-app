b1b3211ef2511c09fdb44c3c8e8d58472bdd37ef
Akeko. <artapp.theo@gmail.com>
2024-04-28 17:03:34 +0200

    feat(404): add 404 error

--- client/src/pages/error/ErrorPage.tsx
+++ client/src/pages/error/ErrorPage.tsx
@@ -1,5 +1,17 @@
+import { LinkComponent } from "../../components/ui";
+
 const ErrorPage = () => {
-  return <div>404 : Page non trouvée !</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <img
+        src="/assets/img/animations/ThinkAnim.gif"
+        className="w-full max-h-60 object-contain"
+        alt="Flami qui te dit bonjour avec sa main."
+      />
+      <h1 className="text-2xl font-bold text-center ">Je pense que tu t'es perdu(e) ?</h1>
+      <LinkComponent to="/" variant={"primary"}>Retour à l'accueil</LinkComponent>
+    </div>
+  );
 };
 
 export default ErrorPage;
